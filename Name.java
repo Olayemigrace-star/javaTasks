73ff326508a0a63a8f27de90dd6de22bf6a40688
Grace Olayemi <olayemigrace29@gmail.com>
2026-04-03 17:23:19 +0100

    test

---
+++ Name.java
@@ -0,0 +1,7 @@
+public class Name{ 
+
+    public static void main(String[] args){
+
+        System.out.print("Grace\n Olayemi\n 2026\n");
+     }
+}
---
+++ Numbers.java
@@ -0,0 +1,7 @@
+ public class Numbers{ 
+
+    public static void main(String[] args){
+
+        System.out.print("35, 44,56,89,2 \n");
+     }
+}
