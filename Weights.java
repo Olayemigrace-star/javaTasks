d39203e9c9d75eb9ee4bb27728518d086e5f0746
Grace Olayemi <olayemigrace29@gmail.com>
2026-04-03 18:26:28 +0100

    Welcome

---
+++ Weights.java
@@ -0,0 +1,18 @@
+ public class Weights{ 
+
+    public static void main(String[] args){
+      
+      double Weight1 = 35.4;
+      double Weight2 = 45.7;
+      double Weight3 = 45.9; 
+      double Weight4 = 102.0;
+      double Weight5 = 40.23;
+      
+
+     System.out.print(Weight1 + "kg\n"); 
+     System.out.print(Weight2 + "kg\n");
+     System.out.print(Weight3 + "kg\n");
+     System.out.print(Weight4 + "kg\n");
+     System.out.print(Weight5 + "kg\n");
+     }
+}
--- Welcome.java
+++ Welcome.java
@@ -2,6 +2,6 @@ public class Welcome{
 
     public static void main(String[] args){
 
-        System.out.println("Hello Tinubu!!!");
+        System.out.printf("Nigeria became independent in %d and is full of great minds", 1960);
      }
 }
